9f628ff6400f99ea7bc3dbd3300a4eb0f7c58c83
dhanush <tsdhanush169@gmail.com>
2025-03-25 14:03:26 +0530

    first commit

---
+++ user_model/authentication.py
@@ -0,0 +1,38 @@
+import jwt
+from rest_framework import authentication, exceptions
+from user_model.models import User
+from django.utils import timezone
+from datetime import datetime
+from rest_framework.response import Response
+from django.conf import settings
+
+class IsAuthenticated(authentication.BaseAuthentication):
+    def authenticate_header(self, request):
+        return "Invalid token"
+
+    def authenticate(self, request):
+        try:
+            token = request.headers.get('Authorization', '').split(' ')[1]
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            user_id = payload.get('id')
+            user = User.objects.get(pk=user_id)
+            if isinstance(user, User):
+                return (user, token)
+            return Response({"error": "No users found"}, status=404)
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed("Token has expired")
+        except jwt.exceptions.InvalidTokenError:
+            raise exceptions.AuthenticationFailed("Invalid token")
+        except IndexError:
+            raise exceptions.AuthenticationFailed("No Token Provided")
+        except Exception as e:
+            print(e)
+            raise exceptions.AuthenticationFailed("An error occurred while decoding token")
+
+def generate_token(user):
+    exp_date = datetime.now() + timezone.timedelta(days=365)
+    return jwt.encode({
+        'id': user.id,
+        'user_type': user.user_type,
+        'exp': exp_date.timestamp()
+    }, settings.SECRET_KEY, algorithm='HS256')
---
+++ user_model/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class User(models.Model):
+    full_name = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=20, default="Changeme@123", blank=True)
+    phone_number = models.CharField(max_length=10,default="0000000000")
+    dob = models.DateField()
+    gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')])
+    height = models.CharField(max_length=10)  # Example: "170 cm"
+    weight = models.CharField(max_length=10)  # Example: "70 kg"
+    city = models.CharField(max_length=255)
+    medical_history = models.TextField(blank=True, null=True)
+    genetic_predisposition = models.TextField(blank=True, null=True)  # Store comma-separated values (e.g., "Cancer, Diabetes, Obesity")
+    def __str__(self):
+        return self.name
